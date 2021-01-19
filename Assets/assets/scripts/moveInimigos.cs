c702f8e7601ad0eb4f06ca88ad3705932abec425
LorenaGoes <77701515+LorenaGoes@users.noreply.github.com>
2021-01-19 19:17:04 -0300

    primeiro commit

---
+++ Assets/assets/scripts/moveInimigos.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveInimigos: MonoBehaviour {
+    private float contMov = 0;
+    gamemaneger gManager;
+    GameObject inObj;
+
+    // Use this for initialization
+    void Start()
+    {
+        gManager = GameObject.Find("GameManager").GetComponent<gamemaneger>();
+        inObj = GameObject.Find("Inimigos");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        contMov += Time.deltaTime;
+
+        if (contMov > 1)
+        {
+            if (gManager.bateuParede == false)
+            {
+                if (gManager.desce == false)
+                {
+                    transform.position += new Vector3(.2f, 0, 0);
+                    contMov = 0;
+                }
+                else
+                {
+                    transform.position += new Vector3(.2f, 0, 0);
+                    inObj.transform.position += new Vector3(0, -1, 0);
+                    gManager.desce = false;
+                    contMov = 0;
+                }
+            }
+            else if (gManager.bateuParede == true)
+            {
+                if (gManager.desce == false)
+                {
+                    transform.position += new Vector3(-.2f, 0, 0);
+                    contMov = 0;
+                }
+                else
+                {
+                    transform.position += new Vector3(-.2f, 0, 0);
+                    inObj.transform.position += new Vector3(0, -1, 0);
+                    gManager.desce = false;
+                    contMov = 0;
+                }
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D outro)
+    {
+        if (outro.gameObject.tag == "paredeDir")
+        {
+            gManager.bateuParede = true;
+            gManager.desce = true;
+        }
+        else if (outro.gameObject.tag == "paredeEsq")
+        {
+            gManager.bateuParede = false;
+            gManager.desce = true;
+        }
+    }
+}
---
+++ Assets/assets/scripts/movePlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movePlayer : MonoBehaviour {
+
+    public float movMulti;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float posX = transform.position.x + Input.GetAxisRaw("Horizontal") * Time.deltaTime * movMulti;
+        transform.position = new Vector3(Mathf.Clamp(posX, -10, 10), -4.3f, 0);
+    }
+}
